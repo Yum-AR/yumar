812ee16f6eb366d8fbf7d12a89dd82ba30caa4f0
willi6m7 <willi6m7@mail.uc.edu>
2022-11-09 14:23:28 -0500

    update NextAuth, add email provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from 'next-auth/providers/google';
+import EmailProvider from 'next-auth/providers/email';
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
@@ -23,6 +25,10 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
+    EmailProvider({
+      server: process.env.MAIL_SERVER,
+      from: 'NextAuth.js <no-reply@example.com>'
+    })
     // ...add more providers here
   ],
 };
