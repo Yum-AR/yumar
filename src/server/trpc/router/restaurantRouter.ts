04d3332e44610b345143d652ac3505634706f04b
ozerbt <54027925+ozerbt@users.noreply.github.com>
2022-11-24 22:56:41 -0500

    created trpc router

---
+++ src/server/trpc/router/restaurantRouter.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod';
+import { router, publicProcedure } from "../trpc";
+
+export const restaurantRouter = router({
+    getRestaurant: publicProcedure.input(z.object({ isApproved: z.boolean(), restaurantId: z.string().optional() })).query(({ input, ctx }) => {
+        let where = {};
+        if (input.restaurantId) {
+            where = {
+                isApproved: input.isApproved,
+                restaurantId: parseInt(input.restaurantId),
+            }
+        } else {
+            where = {
+                isApproved: input.isApproved
+            }
+        }
+
+        return ctx.prisma.restaurant.findMany({
+            where,
+            include: {
+                RestaurantAddress: true,
+                RestaurantInformation: true,
+                RestaurantSettings: true,
+            }
+        })
+    })
+})
