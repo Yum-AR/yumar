749394d99f1039ca23c0483e4ccbaf821b100cce
ozerbt <54027925+ozerbt@users.noreply.github.com>
2022-11-24 22:57:23 -0500

    imported restaurant router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,9 +1,11 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { exampleRouter } from "./example";
+import { restaurantRouter } from "./restaurantRouter";
 
 export const appRouter = router({
   example: exampleRouter,
+  restaurant: restaurantRouter,
   auth: authRouter,
 });
 
