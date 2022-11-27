eb973e50af5a20fa16b3197580843c52e94026ae
ozerbt <54027925+ozerbt@users.noreply.github.com>
2022-11-26 19:53:49 -0500

    removed unused api route

--- src/server/trpc/router/restaurantRouter.ts
+++ src/server/trpc/router/restaurantRouter.ts
@@ -1,28 +1,30 @@
 import { z } from 'zod';
-import { router, publicProcedure } from "../trpc";
+import { publicProcedure, router } from '../trpc';
 
 export const restaurantRouter = router({
-    getRestaurant: publicProcedure.input(z.object({ isApproved: z.boolean(), restaurantId: z.string().optional() })).query(({ input, ctx }) => {
-        let where = {};
-        if (input.restaurantId) {
-            where = {
-                isApproved: input.isApproved,
-                restaurantId: parseInt(input.restaurantId),
-            }
-        } else {
-            where = {
-                isApproved: input.isApproved
-            }
-        }
+  getRestaurant: publicProcedure.input(z.object({
+    isApproved: z.boolean(), restaurantId: z.string().optional(),
+  })).query(({ input, ctx }) => {
+    let where = {};
+    if (input.restaurantId) {
+      where = {
+        isApproved: input.isApproved,
+        restaurantId: parseInt(input.restaurantId),
+      };
+    } else {
+      where = {
+        isApproved: input.isApproved,
+      };
+    }
 
-        return ctx.prisma.restaurant.findMany({
-            where,
-            include: {
-                RestaurantAddress: true,
-                RestaurantInformation: true,
-                RestaurantSettings: true,
-                MenuItems: true,
-            }
-        })
-    })
-})
+    return ctx.prisma.restaurant.findMany({
+      where,
+      include: {
+        RestaurantAddress: true,
+        RestaurantInformation: true,
+        RestaurantSettings: true,
+        MenuItems: true,
+      },
+    });
+  }),
+});
